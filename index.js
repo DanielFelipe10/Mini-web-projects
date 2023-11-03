b5e5358980f63fa647df25a9839eea56a9cc8d00
DanielFelipe10 <daniel.tovart@uniagustiniana.edu.co>
2023-11-03 18:15:41 -0500

    add code to change color randomly

---
+++ index.js
@@ -0,0 +1,16 @@
+const cards = document.querySelectorAll('.card');
+
+
+function generateColor(){
+    const char = "0123456789ABCDEF";
+    let color = "#";
+    for(let i = 0; i < 6; i++){
+        color+= char[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
+
+cards.forEach(card => {
+    const aleatoryColor = generateColor();
+    card.style.backgroundColor = aleatoryColor;
+})
