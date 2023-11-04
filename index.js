57968d1eca0ed21ec8d6273d95f4163f2eb8ada6
DanielFelipe10 <daniel.tovart@uniagustiniana.edu.co>
2023-11-03 20:12:44 -0500

    add image and animation for main links

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cards = document.querySelectorAll('.card');
 
 function generateColor(){
     const char = "0123456789ABCDEF";
-    let color = "#";
-    for(let i = 0; i < 6; i++){
+    let color = "#0";
+    for(let i = 0; i < 5; i++){
         color+= char[Math.floor(Math.random()*16)];
     }
     return color;
